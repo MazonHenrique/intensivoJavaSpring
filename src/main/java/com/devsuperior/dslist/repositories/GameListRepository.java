3e87b246b884f4cf86d2956db309a7a1b7097e69
MazonHenrique <hmazon87@yahoo.com>
2023-05-12 21:50:45 -0300

    Move games on list

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -1,10 +1,17 @@
 package com.devsuperior.dslist.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dslist.entities.GameList;
 
 public interface GameListRepository extends JpaRepository<GameList, Long> {
 	//interface extendida do JPA para fazer iteração com banco de dados
 	//passado o tipo (GameList) e o tipo da variavel do Id(nesse caso long)
+	
+	@Modifying
+	@Query(nativeQuery = true, 
+	value = "UPDATE tb_belonging SET position = :newPosition WHERE list_id = :listId AND game_id = :gameId")
+	void updateBelongingPosition(Long listId, Long gameId, Integer newPosition);
 }
